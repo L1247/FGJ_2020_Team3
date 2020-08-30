f87dff9ad08d0ea60617fbae95426654717a30c3
rStar <starlove124760@gmail.com>
2020-08-30 10:49:35 +0800

    調整噴火動畫

---
+++ Assets/Game/Scripts/Dragon/Dragon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragon : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FireBallAfter()
+    {
+        Debug.Log($"FrieBallAfter");
+        _animator.SetTrigger("DragonFire");
+    }
+}
