c43b10de89cd95b439eeca5cdcbadf2463a992a7
rStar <starlove124760@gmail.com>
2020-08-30 13:08:44 +0800

    Added 對話系統結束

--- Assets/Game/Scripts/DialogSystem/DialogManager.cs
+++ Assets/Game/Scripts/DialogSystem/DialogManager.cs
@@ -1,14 +1,46 @@
 using System;
+using DG.Tweening;
+using Game.Scripts.Utility.TextUtility;
+using UniRx;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace FGJ2020_Team3.DialogSystem
 {
     public class DialogManager : MonoBehaviour
     {
         public DialogSystem dialogSystemCharacter , DialogSystemWitch;
+        public string       NextScene;
+
+
         private void Start()
         {
             dialogSystemCharacter.CallNext();
+            dialogSystemCharacter
+                .GetDialogEnd
+                .Subscribe(unit =>
+                {
+                    // Delay
+                    Observable.Timer(TimeSpan.FromSeconds(1.5f))
+                              .Subscribe(l => CallCharacterMove());
+                });
+        }
+
+        private void CallCharacterMove()
+        {
+            DialogSystemWitch.GetComponentInChildren<CanvasGroup>().alpha = 0;
+            dialogSystemCharacter.GetComponentInChildren<CanvasGroup>().alpha = 0;
+            DialogSystemWitch.transform.DOMoveY(10 , 1)
+                                 .OnComplete(() =>
+                                 {
+                                     dialogSystemCharacter.transform.DOMoveY(10 , 1)
+                                                          .OnComplete(() => ChangeScene());
+                                 });
+        }
+
+        private void ChangeScene()
+        {
+            SceneManager.LoadScene(NextScene);
         }
 
         public void StartDialog()
--- Assets/Game/Scripts/DialogSystem/DialogSystem.cs
+++ Assets/Game/Scripts/DialogSystem/DialogSystem.cs
@@ -8,10 +8,12 @@ namespace FGJ2020_Team3.DialogSystem
 {
     public class DialogSystem : MonoBehaviour
     {
-        [SerializeField] private List<Dialog>     _dialogs;
-        [SerializeField] private UITextTypeWriter _textTypeWriter;
-        [SerializeField] private UITextTypeWriter _otherTextTypeWriter;
-        [SerializeField] private Canvas           _canvas;
+        [SerializeField] private List<Dialog>      _dialogs;
+        [SerializeField] private UITextTypeWriter  _textTypeWriter;
+        [SerializeField] private UITextTypeWriter  _otherTextTypeWriter;
+        [SerializeField] private Canvas            _canvas;
+        public                   IObservable<Unit> GetDialogEnd => _onDialogEnd;
+        private                  Subject<Unit>     _onDialogEnd = new Subject<Unit>();
         
         private                  int              _diglogIndex;
 
@@ -20,10 +22,9 @@ namespace FGJ2020_Team3.DialogSystem
             _otherTextTypeWriter.GetTypeEnd
                                 .Subscribe(unit =>
                                 {
-                                    if(_diglogIndex < _dialogs.Count)
-                                        CallNext();
-                                });
-            
+                                    if(_diglogIndex < _dialogs.Count) CallNext();
+                                }).AddTo(gameObject);
+
         }
 
         public void CallNext()
@@ -32,6 +33,8 @@ namespace FGJ2020_Team3.DialogSystem
             var dialog = _dialogs[_diglogIndex];
             _textTypeWriter.CallNextEffect(dialog.Context);
             _diglogIndex++;
+            if(_diglogIndex >= _dialogs.Count)
+               _textTypeWriter.GetTypeEnd.Subscribe(unit => _onDialogEnd.OnNext(Unit.Default));
         }
     }
 
