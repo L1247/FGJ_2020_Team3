3b2b0bf68a825fa9400b3d833a3c26dc54ff987a
rStar <starlove124760@gmail.com>
2020-08-30 13:48:57 +0800

    增加多拉A夢BGM

---
+++ Assets/Game/Scripts/BattleManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BattleManager : MonoBehaviour
+{
+    [SerializeField] private string reloadSceneName;
+    
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(reloadSceneName);
+    }
+}
