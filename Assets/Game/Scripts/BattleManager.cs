192954fbf83cf4b71c9ea4b534a63a1215c0a7a7
rStar <starlove124760@gmail.com>
2020-08-30 14:20:11 +0800

    串接場景完成

--- Assets/Game/Scripts/BattleManager.cs
+++ Assets/Game/Scripts/BattleManager.cs
@@ -6,8 +6,19 @@ using UnityEngine.SceneManagement;
 public class BattleManager : MonoBehaviour
 {
     [SerializeField] private string reloadSceneName;
-    
-    public void ReloadScene()
+    [SerializeField] private string nextScene;
+
+    public void DelayCallLoadNextScene()
+    {
+        Invoke("LoadNextScene" , 7);
+    }
+
+    void LoadNextScene()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
+    void ReloadScene()
     {
         SceneManager.LoadScene(reloadSceneName);
     }
