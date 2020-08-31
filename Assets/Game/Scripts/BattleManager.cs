446cffd2839e51890c3c557d98d2c430e1b66fad
rStar <starlove124760@gmail.com>
2020-08-31 23:43:17 +0800

    Fixed 重開場景以及重讀遊戲不會重製變數的問題
    編輯器發生

--- Assets/Game/Scripts/BattleManager.cs
+++ Assets/Game/Scripts/BattleManager.cs
@@ -8,20 +8,19 @@ public class BattleManager : MonoBehaviour
 {
     [SerializeField] private string       reloadSceneName;
     [SerializeField] private string       nextScene;
-    public           bool         _doesnotSeenOpeningAnimation; //尚未播放
+    private static           bool         _doesnotSeenOpeningAnimation;
     [SerializeField] private GameObject[] HideObjs;
     [SerializeField] private Animator     Witch ,   Opening , Dragon , Gem;
     [SerializeField] private GameObject   FakeGem , RealGem;
     
     private void Start()
     {
-        if (_doesnotSeenOpeningAnimation == true) //
+        if (_doesnotSeenOpeningAnimation == false)
         {
             foreach (var hideObj in HideObjs)
             {
                 hideObj.SetActive(true);
             }
-            Witch.gameObject.SetActive(true);
 
             // 女巫變身
             WitchChange();
@@ -61,7 +60,7 @@ public class BattleManager : MonoBehaviour
 
     public void CallDragonFallAnimation()
     {
-        RealGem.gameObject.SetActive(true); 
+        RealGem.gameObject.SetActive(true);
         FakeGem.gameObject.SetActive(false);
         Dragon.gameObject.SetActive(true);
         Dragon.transform.position += Vector3.up * 10; 
@@ -73,5 +72,11 @@ public class BattleManager : MonoBehaviour
         RealGem.gameObject.SetActive(false);
         FakeGem.gameObject.SetActive(true);
         Gem.Play("Gem");       
+    }
+
+    private void OnApplicationQuit()
+    {
+        Debug.Log($"Quit");
+        _doesnotSeenOpeningAnimation = false;
     }
 }
